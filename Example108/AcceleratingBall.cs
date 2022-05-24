e251648a18b6e73644866ef59b02c6c634e0d526
HexaBlitz <97612761+SpidErza@users.noreply.github.com>
2022-05-24 13:10:10 +0200

    Movement

---
+++ Example108/AcceleratingBall.cs
@@ -0,0 +1,67 @@
+using System.Numerics; // Vector2
+using Raylib_cs; // Color
+
+/*
+In this class, we have the properties:
+
+- Vector2  Position
+- float    Rotation
+- Vector2  Scale
+
+- Vector2 TextureSize
+- Vector2 Pivot
+- Color Color
+
+Methods:
+
+- AddChild(Node child)
+- RemoveChild(Node child, bool keepAlive = false)
+*/
+
+namespace Movement
+{
+	class AcceleratingBall : SpriteNode
+	{
+		// your private fields here (add Velocity, Acceleration, and MaxSpeed)
+
+
+		// constructor + call base constructor
+		public AcceleratingBall() : base("resources/ball.png")
+		{
+			Position = new Vector2(Settings.ScreenSize.X / 2, Settings.ScreenSize.Y / 4);
+			Color = Color.RED;
+		}
+
+		// Update is called every frame
+		public override void Update(float deltaTime)
+		{
+			Move(deltaTime);
+			WrapEdges();
+		}
+
+		// your own private methods
+		private void Move(float deltaTime)
+		{
+			// TODO implement
+			// Position += Velocity * deltaTime;
+
+			// accelerate your ball (40, 30) every frame
+			// limit to a maximum speed of 1000 pixels/second
+		}
+
+		private void WrapEdges()
+		{
+			float scr_width = Settings.ScreenSize.X;
+			float scr_height = Settings.ScreenSize.Y;
+			float spr_width = TextureSize.X;
+			float spr_heigth = TextureSize.Y;
+
+			// TODO implement...
+			if (Position.X > scr_width)
+			{
+				// ...
+			}
+		}
+
+	}
+}
